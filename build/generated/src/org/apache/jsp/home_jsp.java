8d777f7e98028324748a4aa573c7129ee5fd66c5
Timothy Cox <cobalt111@hotmail.com>
2017-02-06 14:33:06 -0500

    More aesthetic changes

---
+++ build/generated/src/org/apache/jsp/home_jsp.java
@@ -0,0 +1,233 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class home_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_import_url_nobody;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_c_import_url_nobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_c_import_url_nobody.release();
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      if (_jspx_meth_c_import_0(_jspx_page_context))
+        return;
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("        <div class=\"page-header text-center black margin-none\">\r\n");
+      out.write("            <h2 class=\"white\">Faculty Website</h2>\r\n");
+      out.write("        </div>\r\n");
+      out.write("      \r\n");
+      out.write("            <!-- Collect the nav links, forms, and other content for toggling -->\r\n");
+      out.write("            <div class=\"navbar black\" >\r\n");
+      out.write("                <ul class=\"nav navbar-nav navbar-left black\">\r\n");
+      out.write("                    <li>\r\n");
+      out.write("                        <a href=\"about.html\">Create Account</a>\r\n");
+      out.write("                    </li>\r\n");
+      out.write("                    <li>\r\n");
+      out.write("                        <a href=\"contact.html\">Login</a>\r\n");
+      out.write("                    </li>\r\n");
+      out.write("                </ul>    \r\n");
+      out.write("                   \r\n");
+      out.write("            <!-- /.navbar-collapse -->\r\n");
+      out.write("        </div>\r\n");
+      out.write("    \r\n");
+      out.write("\r\n");
+      out.write("    <!-- Page Content -->\r\n");
+      out.write("    ");
+      if (_jspx_meth_c_import_1(_jspx_page_context))
+        return;
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("    <div class=\"container\">\r\n");
+      out.write("        <div class=\"row\">\r\n");
+      out.write("             <div class=\"col-lg-8 col-lg-offset-3 col-md-10 col-md-offset-1\">\r\n");
+      out.write("           \r\n");
+      out.write("            <img src=\"fonts/researcher.jpg\" alt=Researcher\" height=\"420\" width=\"420\">\r\n");
+      out.write("            \r\n");
+      out.write("            <p class=\"lead\">Peter is a PhD student of Anita</p>\r\n");
+      out.write("            <p class=\"lead\"><b>Research Projects</b></p>   \r\n");
+      out.write("         <a href=\"researchproject.jsp\">Mobile Apps Security<br></a>\r\n");
+      out.write("         <a href=\"researchproject.jsp\">Cloud Security<br></a>\r\n");
+      out.write("       <a href=\"researchproject.jsp\">Policy Advisor</a>\r\n");
+      out.write("       <p></p>\r\n");
+      out.write("       <p class=\"lead\"><b>Publications</b></p>\r\n");
+      out.write("       <p>Chorpita, B. F., & Daleiden, E. L. (2014). Structuring the collaboration of science and service in pursuit <br>of a shared vision. <i>Journal of Clinical Child & Adolescent Psychology</i>, 43(2), 323-338.<br></p>\r\n");
+      out.write("      <p>Chorpita, B. F., Bernstein, A., & Daleiden, E. L. (2008). Driving with roadmaps<br> and dashboards: Using information resources to structure the decision models in service organizations. <i>Administration and Policy in Mental Health and Mental Health Services Research</i>, 35(1-2), 114-123.</p>   \r\n");
+      out.write("\t    </div>\t\t\r\n");
+      out.write("        </div>\r\n");
+      out.write("    </div>\r\n");
+      out.write("    \r\n");
+      out.write("    ");
+      if (_jspx_meth_c_import_2(_jspx_page_context))
+        return;
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("    \r\n");
+      if (_jspx_meth_c_import_3(_jspx_page_context))
+        return;
+      out.write('\r');
+      out.write('\n');
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+
+  private boolean _jspx_meth_c_import_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:import
+    org.apache.taglibs.standard.tag.rt.core.ImportTag _jspx_th_c_import_0 = (org.apache.taglibs.standard.tag.rt.core.ImportTag) _jspx_tagPool_c_import_url_nobody.get(org.apache.taglibs.standard.tag.rt.core.ImportTag.class);
+    _jspx_th_c_import_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_import_0.setParent(null);
+    _jspx_th_c_import_0.setUrl("header.html");
+    int[] _jspx_push_body_count_c_import_0 = new int[] { 0 };
+    try {
+      int _jspx_eval_c_import_0 = _jspx_th_c_import_0.doStartTag();
+      if (_jspx_th_c_import_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_c_import_0[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_c_import_0.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_c_import_0.doFinally();
+      _jspx_tagPool_c_import_url_nobody.reuse(_jspx_th_c_import_0);
+    }
+    return false;
+  }
+
+  private boolean _jspx_meth_c_import_1(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:import
+    org.apache.taglibs.standard.tag.rt.core.ImportTag _jspx_th_c_import_1 = (org.apache.taglibs.standard.tag.rt.core.ImportTag) _jspx_tagPool_c_import_url_nobody.get(org.apache.taglibs.standard.tag.rt.core.ImportTag.class);
+    _jspx_th_c_import_1.setPageContext(_jspx_page_context);
+    _jspx_th_c_import_1.setParent(null);
+    _jspx_th_c_import_1.setUrl("filler.html");
+    int[] _jspx_push_body_count_c_import_1 = new int[] { 0 };
+    try {
+      int _jspx_eval_c_import_1 = _jspx_th_c_import_1.doStartTag();
+      if (_jspx_th_c_import_1.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_c_import_1[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_c_import_1.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_c_import_1.doFinally();
+      _jspx_tagPool_c_import_url_nobody.reuse(_jspx_th_c_import_1);
+    }
+    return false;
+  }
+
+  private boolean _jspx_meth_c_import_2(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:import
+    org.apache.taglibs.standard.tag.rt.core.ImportTag _jspx_th_c_import_2 = (org.apache.taglibs.standard.tag.rt.core.ImportTag) _jspx_tagPool_c_import_url_nobody.get(org.apache.taglibs.standard.tag.rt.core.ImportTag.class);
+    _jspx_th_c_import_2.setPageContext(_jspx_page_context);
+    _jspx_th_c_import_2.setParent(null);
+    _jspx_th_c_import_2.setUrl("filler.html");
+    int[] _jspx_push_body_count_c_import_2 = new int[] { 0 };
+    try {
+      int _jspx_eval_c_import_2 = _jspx_th_c_import_2.doStartTag();
+      if (_jspx_th_c_import_2.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_c_import_2[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_c_import_2.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_c_import_2.doFinally();
+      _jspx_tagPool_c_import_url_nobody.reuse(_jspx_th_c_import_2);
+    }
+    return false;
+  }
+
+  private boolean _jspx_meth_c_import_3(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:import
+    org.apache.taglibs.standard.tag.rt.core.ImportTag _jspx_th_c_import_3 = (org.apache.taglibs.standard.tag.rt.core.ImportTag) _jspx_tagPool_c_import_url_nobody.get(org.apache.taglibs.standard.tag.rt.core.ImportTag.class);
+    _jspx_th_c_import_3.setPageContext(_jspx_page_context);
+    _jspx_th_c_import_3.setParent(null);
+    _jspx_th_c_import_3.setUrl("footer.html");
+    int[] _jspx_push_body_count_c_import_3 = new int[] { 0 };
+    try {
+      int _jspx_eval_c_import_3 = _jspx_th_c_import_3.doStartTag();
+      if (_jspx_th_c_import_3.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_c_import_3[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_c_import_3.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_c_import_3.doFinally();
+      _jspx_tagPool_c_import_url_nobody.reuse(_jspx_th_c_import_3);
+    }
+    return false;
+  }
+}
--- web/news.jsp
+++ web/news.jsp
@@ -34,11 +34,11 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-3 col-md-10 col-md-offset-1">
-            <h2 class="section-heading">Welcome Faculty!</h2>
-            <p class="lead">If you have an account click on the login link.<br>
-                            Otherwise click on the create account link.</p>
-       
-	    </div>		
+           
+      <p class="lead">News</p>
+      <p>Chorpita, B. F., & Daleiden, E. L. (2014). Structuring the collaboration of science and service in pursuit <br>of a shared vision. <i>Journal of Clinical Child & Adolescent Psychology</i>, 43(2), 323-338.<br></p>
+      <p>Chorpita, B. F., Bernstein, A., & Daleiden, E. L. (2008). Driving with roadmaps<br> and dashboards: Using information resources to structure the decision models in service organizations. <i>Administration and Policy in Mental Health and Mental Health Services Research</i>, 35(1-2), 114-123.</p>   
+            </div>		
         </div>
     </div>
     
